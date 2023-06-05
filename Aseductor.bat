74454ce0603a946e765e9299c37db243ec5bc509
Pando <81963564+Perndoe@users.noreply.github.com>
2023-06-05 22:56:02 +0700

    Improved version
    Just searching the internet for more code to use

---
+++ Aseductor.bat
@@ -0,0 +1,34 @@
+@echo off
+
+::Download Aseprite Source code
+echo Link of the Aseprite version you want to install:
+
+timeout /t 2 /nobreak >nul
+start https://github.com/aseprite/aseprite/releases/
+
+set /p input="> "
+powershell -Command "Invoke-WebRequest %input% -OutFile %USERPROFILE%\Downloads\Compressed\aseprite.zip"
+@echo on
+
+::Extract Source code
+powershell Expand-Archive %USERPROFILE%\Downloads\Compressed\aseprite.zip -DestinationPath %USERPROFILE%\Downloads\Compressed\Aseprite
+
+::Call Developer Command Prompt
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -arch=x64
+@echo on
+
+::Copy core files
+cd C:\
+md "aseprite"\"build"
+xcopy "%USERPROFILE%\Downloads\Compressed\Skia Windows" "C:\deps\" /E
+xcopy "%USERPROFILE%\Downloads\Compressed\Aseprite" "C:\aseprite\" /E
+
+cd C:\aseprite\build
+
+::Build Aseprite
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLAF_BACKEND=skia -DSKIA_DIR=C:\deps\skia -DSKIA_LIBRARY_DIR=C:\deps\skia\out\Release-x64 -DSKIA_LIBRARY=C:\deps\skia\out\Release-x64\skia.lib -G Ninja ..
+
+::Export build
+ninja aseprite
+
+pause >nul
